fffb7abb8cb84a7a7bc14de7e61100f5ab393110
bernice <bernice@hubtel.com>
2022-10-26 12:05:12 +0000

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <div className="absolute bottom-0 text-center w-11/12">
-      Copyright 2022 My NEXT App
-    </div>
-  );
+  return <div className=" text-center mt-24">Copyright 2022 My NEXT App</div>;
 }
 
 export default Footer;
