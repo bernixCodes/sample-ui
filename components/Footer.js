564a2b25f0800cd9b5230f1851a5e073d60600b8
bernice <bernice@hubtel.com>
2022-10-26 12:03:12 +0000

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="absolute bottom-1 text-center w-11/12">
+    <div className="absolute bottom-0 text-center w-11/12">
       Copyright 2022 My NEXT App
     </div>
   );
